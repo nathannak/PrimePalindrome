5bf31fc4279fe92918d9b301ebc408be1d1b28a7
nn <a%liKh2&>
2019-12-07 23:38:56 -0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+    //Find the smallest prime palindrome greater than or equal to N.
+    
+    public static void main(String[] args) {
+
+        System.out.println( primePalindrome(107) );
+    }
+
+
+    public static int primePalindrome(int N) {
+        if (8 <= N && N <= 11) return 11;
+        for (int x = 1; x < 100000; x++) {
+            String s = Integer.toString(x), r = new StringBuilder(s).reverse().toString();
+            int y = Integer.parseInt(s + r.substring(1));
+            if (y >= N && isPrime(y)) return y;
+        }
+        return -1;
+    }
+
+    public static Boolean isPrime(int x) {
+        if (x < 2 || x % 2 == 0) return x == 2;
+        for (int i = 3; i * i <= x; i += 2)
+            if (x % i == 0) return false;
+        return true;
+    }
+
+}
